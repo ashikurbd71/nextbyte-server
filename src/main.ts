fa6376ca92cf797d886919114ab72de4929e7c1d
ashikurovibd71 <ashikurovi2003@gmail.com>
2025-12-11 23:19:38 +0600

    update

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-let cachedApp: NestExpressApplication;
 
 async function bootstrap() {
-  if (!cachedApp) {
-    const app = await NestFactory.create<NestExpressApplication>(AppModule);
-    app.enableCors();
-    cachedApp = app;
-  }
-  return cachedApp;
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
+bootstrap();
 
-// Vercel handler
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const app = await bootstrap();
-  app.getHttpAdapter().getInstance()(req, res);
-}
+// Export default for Vercel serverless
+export default bootstrap;
