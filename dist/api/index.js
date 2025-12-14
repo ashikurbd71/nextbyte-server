6e920bd6b3218b5f6a02e31179ff9f3ffde4f4a7
ashikurovibd71 <ashikurovi2003@gmail.com>
2025-12-14 16:09:20 +0600

    update

--- dist/api/index.js
+++ dist/api/index.js
@@ -3,6 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = handler;
 const get_app_1 = require("../src/get-app");
 async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     if (req.method === 'OPTIONS') {
         res.status(204).end();
         return;
